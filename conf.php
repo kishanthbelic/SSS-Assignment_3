2b77b344f32a8a69e856e522888c3db7c36d1d20
Kishanth Prabakaran <31062472+kishanthbelic@users.noreply.github.com>
2018-05-30 01:18:22 +0530

    .

--- conf.php
+++ conf.php
@@ -4,8 +4,8 @@
 	require_once ("Facebook/autoload.php");
 
 	$fb = new \Facebook\Facebook([
-    	'app_id' => '1863147410391053',
-    	'app_secret' => '3a9f3fcb0bb90db60c96e93e1a39261b',
+    	'app_id' => '256926995048307',
+    	'app_secret' => 'd22a973d4a4a45804bfd33b61450001b',
     	'default_graph_version' => 'v3.0',
     //'default_access_token' => '{access-token}', // optional
 ]);
